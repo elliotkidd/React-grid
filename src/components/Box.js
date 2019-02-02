ef4699659727baf56e1e623233112ceb845dbb2d
Elliot <elliot.kidd89@gmail.com>
2019-02-03 06:17:35 +1000

    Construct layout

---
+++ src/components/Box.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import styled, { css } from "styled-components";
+
+const Box = styled.div`
+  background: #fff;
+  padding: 1rem;
+  text-align: center;
+  margin: 0.5rem 0;
+  border-radius: 3px;
+  border: 1px solid #efefef;
+  box-shaddow: 0 10px 20px hsla(240, 35, 20, 0.095),
+    0 6px 6px hsla(240, 35, 20, 0.115);
+`;
+
+export default Box;
--- src/index.js
+++ src/index.js
@@ -2,17 +2,109 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Grid from "./components/Grid";
 import Column from "./components/Column";
+import Box from "./components/Box";
 
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <Grid justifyContent="center">
-        <Column md="6">Test</Column>
+      <Grid>
+        <Column md={12}>
+          <Box>Twelve</Box>
+        </Column>
       </Grid>
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Grid>
+        <Column md={6}>
+          <Box>Six</Box>
+        </Column>
+        <Column md={6}>
+          <Box>Six</Box>
+        </Column>
+      </Grid>
+      <Grid>
+        <Column md={4}>
+          <Box>Four</Box>
+        </Column>
+        <Column md={4}>
+          <Box>Four</Box>
+        </Column>
+        <Column md={4}>
+          <Box>Four</Box>
+        </Column>
+      </Grid>
+      <Grid>
+        <Column md={3}>
+          <Box>Three</Box>
+        </Column>
+        <Column md={3}>
+          <Box>Three</Box>
+        </Column>
+        <Column md={3}>
+          <Box>Three</Box>
+        </Column>
+        <Column md={3}>
+          <Box>Three</Box>
+        </Column>
+      </Grid>
+      <Grid>
+        <Column md={2}>
+          <Box>Two</Box>
+        </Column>
+        <Column md={2}>
+          <Box>Two</Box>
+        </Column>
+        <Column md={2}>
+          <Box>Two</Box>
+        </Column>
+        <Column md={2}>
+          <Box>Two</Box>
+        </Column>
+        <Column md={2}>
+          <Box>Two</Box>
+        </Column>
+        <Column md={2}>
+          <Box>Two</Box>
+        </Column>
+      </Grid>
+      <Grid>
+        <Column md={1}>
+          <Box>One</Box>
+        </Column>
+        <Column md={1}>
+          <Box>One</Box>
+        </Column>
+        <Column md={1}>
+          <Box>One</Box>
+        </Column>
+        <Column md={1}>
+          <Box>One</Box>
+        </Column>
+        <Column md={1}>
+          <Box>One</Box>
+        </Column>
+        <Column md={1}>
+          <Box>One</Box>
+        </Column>
+        <Column md={1}>
+          <Box>One</Box>
+        </Column>
+        <Column md={1}>
+          <Box>One</Box>
+        </Column>
+        <Column md={1}>
+          <Box>One</Box>
+        </Column>
+        <Column md={1}>
+          <Box>One</Box>
+        </Column>
+        <Column md={1}>
+          <Box>One</Box>
+        </Column>
+        <Column md={1}>
+          <Box>One</Box>
+        </Column>
+      </Grid>
     </div>
   );
 }
